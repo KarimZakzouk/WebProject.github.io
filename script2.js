374f44f540f7560969f8be4b0a93ae869135981f
KarimZakzouk <kokyfarid82@yahoo.com>
2024-08-26 17:21:41 +0300

    Added Details for countries

--- script2.js
+++ script2.js
@@ -1,8 +1,31 @@
 const urlParams = new URLSearchParams(window.location.search);
 const countryCode = urlParams.get('code');
 
-fetch('data.json')
-                .then(response => response.json())
-                .then(countries => {
-                    const country = countries.find(c => c.alpha3Code === countryCode);
-                });
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(countries => {
+            const country = countries.find(c => c.alpha3Code === countryCode);
+            const img = document.getElementById('country-details');
+            const countryImg = document.createElement('div');
+            countryImg.className = 'country-img';
+            countryImg.innerHTML = 
+            `
+                <img src="${country.flags.png}">
+            `
+            img.appendChild(countryImg);
+
+            const info = document.getElementById('country-details');
+            const countryInfo = document.createElement('div');
+            countryInfo.className = 'country-info';
+            countryInfo.innerHTML = 
+            `
+                <h2>${country.name}</h2>
+                <p><b>Population:</b> ${country.population}</p>
+                <p><b>Region:</b> ${country.region}</p>
+                <p><b>Capital:</b> ${country.capital}</p>
+            `
+            info.appendChild(countryInfo);
+
+        })
+});
